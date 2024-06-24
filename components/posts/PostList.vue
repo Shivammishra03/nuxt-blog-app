f577356e473fa1902f57f7e1aaa19172ec856ab3
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-06-24 16:24:56 +0530

    "Refactored PostList component to accept a posts array as a prop and updated index.vue to pass a loadedPosts array to PostList"

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -5,6 +5,10 @@ const props = defineProps({
   isAdmin: {
     type: Boolean,
     default: false
+  },
+  posts: {
+    type: Array,
+    required: true
   }
 })
 
@@ -12,9 +16,7 @@ const props = defineProps({
 
 <template>
     <section class="posts-list">
-        <PostPreview id="1" :is-admin="props.isAdmin" title="Hello Tech," previewText="This is my first post" thumbnail="https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-        <PostPreview id="2" :is-admin="props.isAdmin" title="Hello Tech," previewText="This is my second post" thumbnail="https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-        <PostPreview id="3" :is-admin="props.isAdmin" title="Hello Tech," previewText="This is my third post" thumbnail="https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+        <PostPreview v-for="postsItems in props.posts" :key="postsItems.id" :id="postsItems.id" :is-admin="props.isAdmin" :title="postsItems.title" :previewText="postsItems.previewText" :thumbnail="postsItems.thumbnail"/>
     </section>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,27 @@
 import PostList from '~/components/posts/PostList.vue';
 import PostPreview from '~/components/posts/PostPreview.vue';
 
+const loadedPosts = ref([
+  {
+    id:'1', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id:'2', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id:'3', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+])
+
 </script>
 
 <template>
@@ -14,7 +35,7 @@ import PostPreview from '~/components/posts/PostPreview.vue';
       <PostPreview id="2" title="Hello Tech," previewText="This is my second post" thumbnail="https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
       <PostPreview id="3" title="Hello Tech," previewText="This is my third post" thumbnail="https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
     </section> -->
-    <PostList/>
+    <PostList :posts="loadedPosts"/>
   </div>
 </template>
 
