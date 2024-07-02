488f448cf78421c1729a23af39b4f8f08d565750
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-07-02 19:02:24 +0530

    Added a new paragraph to the About page.

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -6,5 +6,6 @@
     <div class="about-page">
         <h1>About</h1>
         <p>Hi, I'm a developer from the Netherlands. I'm currently working on a nuxt blog app project.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit maxime officia ducimus minima ut assumenda explicabo et aliquid provident aliquam iste animi ea, repellendus ipsum! Maiores assumenda aliquid illum adipisci!</p>
     </div>
 </template>
