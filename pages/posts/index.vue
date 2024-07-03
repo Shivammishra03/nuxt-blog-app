34760d6daf75bec161039f4f601cf90dd43da1eb
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-07-03 21:13:27 +0530

    "Added hardcoded post data to index.vue and passed it as a prop to PostList component"

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,12 +1,33 @@
 <script lang="ts" setup>
 import PostList from '~/components/posts/PostList.vue';
 
+const loadedPosts = ref([
+  {
+    id:'1', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id:'2', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id:'3', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+])
+
 
 </script>
 
 <template>
     <div class="posts-page">
-        <PostList/>
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 
