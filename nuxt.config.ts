4a86a9892aa789a654fd10dfd08cf1b813e908c6
Shivam Mishra <shivammishra221310@gmail.com>
2024-10-04 10:50:06 +0530

    chore: file update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     head: {
       // title: 'Nuxt 3',
@@ -16,5 +17,7 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap'},
       ],
     }
-  }
+  },
+
+  compatibilityDate: '2024-10-01'
 })
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -6,6 +6,27 @@ definePageMeta({
   layout:'admin'
 });
 
+const loadedPosts = ref([
+  {
+    id:'1', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id:'2', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id:'3', 
+    title:'first post', 
+    previewText: 'This is my first post', 
+    thumbnail: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+])
+
 
 </script>
 
@@ -16,7 +37,7 @@ definePageMeta({
         </section>
         <section class="existing-posts">
             <h1>Existing Posts</h1>
-            <PostList isAdmin/>
+            <PostList :isAdmin="true" :posts="loadedPosts"/>
         </section>
     </div>
 </template>
